e8a364284c3d165093150cfeb5864bb6000276b4
RicharSte <Data.Science.NikSte@yandex.ru>
2022-11-23 12:52:29 +0700

    i am srill working on it

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+// /Users/nikolaistepanov/Desktop/test_video.mp4
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+const BufferSize = 10485760
 
 func main() {
-	fmt.Println("Test")
+	file, err := os.Open("/Users/nikolaistepanov/Desktop/test_video.mp4")
+	check(err)
+
+	defer file.Close()
+	file_stat, err := file.Stat()
+	check(err)
+	fmt.Println(int(file_stat.Size()))
 }
