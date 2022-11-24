9495c6bd54078e2b40a6a1914dbd6a75366f4dbd
RicharSte <Data.Science.NikSte@yandex.ru>
2022-11-24 15:34:24 +0700

    few changes

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"sync"
 )
 
 // /Users/nikolaistepanov/Desktop/test_video.mp4
@@ -11,6 +11,10 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func read_chunk(file *os.File, i int, wg *sync.WaitGroup) {
+	// нужно дописать функцию, чтобы она читала трафик
 }
 
 const BufferSize = 10485760
@@ -22,5 +26,17 @@ func main() {
 	defer file.Close()
 	file_stat, err := file.Stat()
 	check(err)
-	fmt.Println(int(file_stat.Size()))
+	file_size := int(file_stat.Size())
+	concurrency := file_size / BufferSize
+
+	if check := file_size % BufferSize; check != 0 {
+		concurrency++
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
+
+	for i := 0; i < concurrency; i++ {
+
+	}
 }
